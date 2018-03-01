92c907fcd2ec1dbddb341d8759fa939f905d2170
John Castillo <JohnluisCastillo@gmail.com>
2018-03-01 11:45:56 -0500

    linked list

---
+++ linkedlist.js
@@ -0,0 +1,62 @@
+class _Node {
+    constructor(value, next) {
+        this.value=value,
+        this.next=next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+            this.head = null;
+    }
+    insertFirst(item){
+        this.head = new _Node(item,this.head);
+    }
+    insertLast(item){
+        if(this.head === null){
+            this.insertFirst(item);
+        } else {
+            let temp = this.head;
+            while(temp.next !== null){
+                temp = temp.next
+            }
+            temp.next = new _Node(item,null)
+        }
+    }
+    remove(item){
+        if(!this.head){
+            return null
+        }
+        if(this.head === item){
+            this.head = this.head.next;
+            return;
+        }
+        let currentNode = this.head;
+
+        let prevNode = this.head;
+
+        while((currentNode !== null) && (currentNode.value !== item)){
+            prevNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        if(currentNode === null){
+            console.log('item not found');
+            return;
+        }
+        prevNode.next = currentNode.next
+    }
+    find(item){
+        let currentNode = this.head;
+        if(!this.head){
+            return null
+        }
+        while(currentNode.value !== item){
+            if(currentNode.next === null){
+                return null
+            } else {
+            currentNode = currentNode.next;
+            }
+        }
+        return currentNode;
+    }
+}
