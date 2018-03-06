3a849d0cdd41a2158b42941dc2d227fffc4f6eba
John Castillo <JohnluisCastillo@gmail.com>
2018-03-06 11:55:20 -0500

    hashmap

---
+++ hashmap.js
@@ -0,0 +1,127 @@
+class HashMap {
+  constructor(initialCapacity = 8) {
+    this.length = 0;
+    this._slots = [];
+    this._capacity = initialCapacity;
+    this._deleted = 0;
+  }
+
+  static _hashString(string) {
+    let hash = 5381;
+    for (let i = 0; i < string.length; i++) {
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      hash = hash & hash;
+    }
+    return hash >>> 0;
+  }
+
+  get(key) {
+    const index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+      throw new Error("Key error");
+    }
+    return this._slots[index].value;
+  }
+
+  set(key, value) {
+    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
+    }
+    const index = this._findSlot(key);
+    this._slots[index] = {
+      key,
+      value,
+      deleted: false
+    };
+    this.length++;
+  }
+
+  remove(key) {
+    const index = this._findSlot(key);
+    const slot = this._slots[index];
+    if (slot === undefined) {
+      throw new Error("Key error");
+    }
+    slot.deleted = true;
+    this.length--;
+    this._deleted++;
+  }
+
+  _findSlot(key) {
+    const hash = HashMap._hashString(key);
+    const start = hash % this._capacity;
+
+    for (let i = start; i < start + this._capacity; i++) {
+      const index = i % this._capacity;
+      const slot = this._slots[index];
+      if (slot === undefined || (slot.key === key && !slot.deleted)) {
+        return index;
+      }
+    }
+  }
+
+  _resize(size) {
+    const oldSlots = this._slots;
+    this._capacity = size;
+    // Reset the length - it will get rebuilt as you add the items back
+    this.length = 0;
+    this._deleted = 0;
+    this._slots = [];
+
+    for (const slot of oldSlots) {
+      if (slot !== undefined && !slot.deleted) {
+        this.set(slot.key, slot.value);
+      }
+    }
+  }
+}
+
+HashMap.MAX_LOAD_RATIO = 0.9;
+HashMap.SIZE_RATIO = 3;
+
+function main() {
+  const lor = new HashMap();
+  lor.set("Hobbit", "Bilbo");
+  lor.set("Hobbit", "Frodo");
+  lor.set("Wizard", "Gandolf");
+  lor.set("Human", "Aragon");
+  lor.set("Elf", "Legolas");
+  lor.set("Maiar", "The Necromancer");
+  lor.set("Maiar", "Sauron");
+  lor.set("RingBearer", "Gollum");
+  lor.set("LadyOfLight", "Galadriel");
+  lor.set("HalfElven", "Arwen");
+  lor.set("Ent", "Treebeard");
+}
+
+function found(hash,currentKey) {
+  for (let i = 0; i < hash._slots.length; i++) {
+    let found = null;
+    console.log(found)
+    // found stays null :( ask mentor)
+    if (hash._slots[i]) {
+      if (hash._slots[i].key === currentKey) {
+        found = true;
+      }
+    } else {
+      found = false;
+    }
+  }
+}
+
+function palindrome(str) {
+  hash = new HashMap();
+  for (let i = 0; i < str.length; i++) {
+    if (found(hash,str[i])) {
+      hash.set(str[i], hash.get(str[i]) + 1);
+    } else {
+      hash.set(str[i], 1);
+    }
+  }
+}
+
+
+console.log(palindrome("racecar"));
+
+main();
